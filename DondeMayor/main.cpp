89bf7d6cdf65cccb4bed76baea7a18918b697567
cbotella <carlosb.n@icloud.com>
2021-10-21 11:37:07 +0200

    DondeMayor 1.0

--- DondeMayor/main.cpp
+++ DondeMayor/main.cpp
@@ -4,11 +4,23 @@
 //
 //  Created by Carlos Botella Navarro on 21/10/21.
 //
-
+//------------------------------------------------
+// Lista <R> --> dondeEstaElMayor() --> R
+//------------------------------------------------
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int dondeEstaElMayor(int * p, int tam){ // Bucle para encontrar la posicion del mayor
+    int mayor = 0;
+    for (int i = 0; i < tam; i++) {
+        if (p[mayor] < p[i]) {
+            mayor = i;
+        }
+    }
+    return mayor;
+}
+int main() {
+    int lista[10] = {1,4,7,8,3,45,6,3,2,9};
+    
+    cout << dondeEstaElMayor(&lista[0], 10) << endl;
 }
